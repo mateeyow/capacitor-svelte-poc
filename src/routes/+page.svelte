a03080132eba9fac74aa2805e033bcff759df403
Matthew Torres <matthew.torres211@gmail.com>
2023-12-22 10:34:32 +0800

    Add example page with tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="p-12 text-center m-4 rounded-md shadow-md bg-slate-100">Hello</div>
