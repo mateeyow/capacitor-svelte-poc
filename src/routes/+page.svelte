1361dbe8b426db3f469fa501d7ed75e779d259ad
Matthew Torres <matthew.torres211@gmail.com>
2023-12-24 00:07:59 +0800

    Add prisma to test for server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
+<!-- <script lang="ts">
+	export let data;
+</script> -->
+
 <div class="p-12 text-center m-4 rounded-md shadow-md bg-slate-100">Hello</div>
+
+<form method="POST" action="?/create">
+	<button class="bg-emerald-400 p-2 text-white rounded-md">Create Notification</button>
+</form>
+<!-- 
+{#if data.notifications.length > 0}
+	<div>
+		<h1>Notifications</h1>
+		{#each data.notifications as notification (notification.id)}
+			<p>{notification.message}</p>
+		{/each}
+	</div>
+{/if} -->
