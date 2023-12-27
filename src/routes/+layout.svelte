ee02e7ad7e30a46f6cc2384df26428a89930ac7d
Matthew Torres <matthew.torres211@gmail.com>
2023-12-27 12:07:22 +0800

    Add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
 	import '../app.css';
 </script>
 
+<nav class="mt-20">
+	<ul class="flex-row flex space-x-2">
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">About</a></li>
+		<li><a href="/contact">Contact</a></li>
+	</ul>
+</nav>
 <slot />
